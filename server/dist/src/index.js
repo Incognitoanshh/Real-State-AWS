9cd1aea249cce7b44bd20d5a1c7467b9b235066d
IncognitoAnshh <amritanshu1403@gmail.com>
2025-06-30 16:22:28 +0530

    added mapbox and authenticationt

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,21 +9,34 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const authMiddleware_1 = require("./middleware/authMiddleware");
+/* ROUTE IMPORT */
+const tenantRoutes_1 = __importDefault(require("./routes/tenantRoutes"));
+const managerRoutes_1 = __importDefault(require("./routes/managerRoutes"));
+const propertyRoutes_1 = __importDefault(require("./routes/propertyRoutes"));
+const leaseRoutes_1 = __importDefault(require("./routes/leaseRoutes"));
+const applicationRoutes_1 = __importDefault(require("./routes/applicationRoutes"));
+/* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-// Middleware
-app.use(body_parser_1.default.json()); // or app.use(express.json());
+app.use(express_1.default.json());
+app.use((0, helmet_1.default)());
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use((0, morgan_1.default)("common"));
+app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
-app.use((0, helmet_1.default)());
-app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use((0, morgan_1.default)('common'));
-// Routes
-app.get('/', (req, res) => {
-    res.send('this is home route');
+/* ROUTES */
+app.get("/", (req, res) => {
+    res.send("This is home route");
 });
-// Server
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.use("/applications", applicationRoutes_1.default);
+app.use("/properties", propertyRoutes_1.default);
+app.use("/leases", leaseRoutes_1.default);
+app.use("/tenants", (0, authMiddleware_1.authMiddleware)(["tenant"]), tenantRoutes_1.default);
+app.use("/managers", (0, authMiddleware_1.authMiddleware)(["manager"]), managerRoutes_1.default);
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
 });
